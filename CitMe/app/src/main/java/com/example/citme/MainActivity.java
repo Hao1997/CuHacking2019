dda9f5d9620884d88b9a97952ccbf51ecc9d80b2
Joshua Obiha <joshua.obiha@hotmail.com>
2019-02-16 12:05:43 -0500

    Add files via upload

---
+++ CitMe/app/src/main/java/com/example/citme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.citme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
