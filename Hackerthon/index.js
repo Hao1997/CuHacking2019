8519f98f78b51b65adb53f2959ac94047386ef56
Joshua Obiha <joshua.obiha@hotmail.com>
2019-02-17 01:35:42 -0500

    Add files via upload

---
+++ Hackerthon/index.js
@@ -0,0 +1,12 @@
+// var dt = new Date();
+// document.getElementById("demo").innerHTML = dt.toLocaleString();
+
+var cities = {   
+        "cities":[
+        {"city":"NewYork"},
+        {"city":"Dublin"}
+    ]
+}
+
+
+console.log(cities.cities[0].city);
